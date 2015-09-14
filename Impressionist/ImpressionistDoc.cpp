f915914b98cd51ed254d9a72902c7f7f02a63997
chenshaoyu1995 <chenshaoyu1995@gmail.com>
2015-09-14 17:21:57 +0800

    curve brush

--- Impressionist/ImpressionistDoc.cpp
+++ Impressionist/ImpressionistDoc.cpp
@@ -19,6 +19,7 @@
 #include "ScatteredLineBrush.h"
 #include "ScatteredPointBrush.h"
 #include "ScatteredCircleBrush.h"
+#include "CurvedBrush.h"
 
 #include "ImageUtils.h"
 #include <vector>
@@ -62,6 +63,8 @@ ImpressionistDoc::ImpressionistDoc()
 		= new ScatteredLineBrush( this, "Scattered Lines" );
 	ImpBrush::c_pBrushes[BRUSH_SCATTERED_CIRCLES]	
 		= new ScatteredCircleBrush(this, "Scattered Circles");
+	ImpBrush::c_pBrushes[BRUSH_CURVED]
+		= new CurvedBrush(this, "Scattered Circles");
 
 	// make one of the brushes current
 	m_pCurrentBrush	= ImpBrush::c_pBrushes[0];
