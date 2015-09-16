3e521699e7b14a3dbd30715caea8677b2fed279a
chenshaoyu1995 <chenshaoyu1995@gmail.com>
2015-09-16 18:43:56 +0800

    Rendering with Triangle Strips

--- Impressionist/CurvedBrush.cpp
+++ Impressionist/CurvedBrush.cpp
@@ -48,7 +48,51 @@ void CurvedBrush::BrushMove(const Point source, const Point target)
 	unsigned char* imageSource = (pDoc->m_ucBitmapBlurred == NULL) ? pDoc->m_ucBitmap : pDoc->m_ucBitmapBlurred;
 
 	vector<pair< pair<int, int>, tuple<unsigned char, unsigned char, unsigned char> > > centers = CurvedBrushHelper::getCurvedBrushPoints(imageSource, pDoc->m_iGradient, pDoc->m_ucPreservedPainting, width, height, source.x, source.y, radius, minStrokeLength, maxStrokeLength, curvatureFilter);
+	
+	
+	
+	//calculate curves tangent
+	double* tangentX = new double[centers.size()];
+	double* tangentY = new double[centers.size()];
+	tangentX[0] = centers[1].first.first - centers[0].first.first;
+	tangentY[0] = centers[1].first.second - centers[0].first.second;
+	tangentX[centers.size() - 1] = centers[centers.size() - 1].first.first - centers[centers.size() - 2].first.first;
+	tangentY[centers.size() - 1] = centers[centers.size() - 1].first.second - centers[centers.size() - 2].first.second;
+	for (int i = 1; i < centers.size()-1;++i)
+	{
+		tangentX[i] = centers[i+1].first.first - centers[i-1].first.first;
+		tangentY[i] = centers[i + 1].first.second - centers[i - 1].first.second;
+	}
+	//calculate normal directions
+	double* normalX = new double[centers.size()];
+	double* normalY = new double[centers.size()];
+	for (int i = 0; i < centers.size(); ++i)
+	{
+		double unit = sqrt(pow(tangentX[i], 2) + pow(tangentY[i], 2));
+		normalX[i] = tangentY[i] / unit;
+		normalY[i] = -tangentX[i] / unit;
+	}
+	//calculate boundary points and draw
+	glBegin(GL_TRIANGLE_STRIP);
+	GLubyte color[4];
+	color[3] = pDoc->getAlpha() * 255;
+	auto color3 = centers[0].second;
+	color[0] = get<0>(color3);
+	color[1] = get<1>(color3);
+	color[2] = get<2>(color3);
+	glColor4ubv(color);
+	for (int i = 0; i < centers.size(); ++i)
+	{
+		glVertex2d(centers[i].first.first + radius*normalX[i], centers[i].first.second + radius*normalY[i]);
+		glVertex2d(centers[i].first.first - radius*normalX[i], centers[i].first.second - radius*normalY[i]);
+	}
+	glEnd();
+	delete tangentX;
+	delete tangentY;
+	delete normalX;
+	delete normalY;
 
+	/*
 	GLubyte color[4];
 	color[3] = pDoc->getAlpha() * 255;
 	for (auto c : centers)
@@ -68,6 +112,7 @@ void CurvedBrush::BrushMove(const Point source, const Point target)
 		}
 		glEnd();
 	}
+	*/
 }
 
 void CurvedBrush::BrushEnd(const Point source, const Point target)
