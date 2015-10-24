b34b7a4d2bbbf00377bbe30534c5e3772230bcd7
chenshaoyu1995 <chenshaoyu1995@gmail.com>
2015-10-24 23:31:58 +0800

    Disable f(d)
    disable it since without log scaling it gives poor result

--- Trace/src/scene/material.cpp
+++ Trace/src/scene/material.cpp
@@ -43,7 +43,8 @@ vec3f Material::shade( Scene *scene, const ray& r, const isect& i ) const
 		double a = 0.25;
 		double b = 0.01;
 		double c = 0.01;
-		double fd = min<double>(1, 1 / (a + b*i.t + c*i.t*i.t));
+		double fd = 1;
+		//double fd = min<double>(1, 1 / (a + b*i.t + c*i.t*i.t));
 
 		sum2 += prod(currlight->getColor(zero),NL*kd+VR * ks)*fd;
 	}
